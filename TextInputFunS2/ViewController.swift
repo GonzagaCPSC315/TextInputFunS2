6a9cdcf9cae0532664cecc9155588279c921da3b
Gina Sprint <sprint@gonzaga.edu>
2020-10-13 16:48:42 -0700

    Initial Commit

---
+++ TextInputFunS2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextInputFunS2
+//
+//  Created by Gina Sprint on 10/13/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
